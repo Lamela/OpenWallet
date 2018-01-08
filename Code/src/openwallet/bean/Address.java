56530cad19ab2375415e8475bc1fd1f178cc46d1
XU Jitao <jitao.xu@insa-rouen.fr>
2018-01-08 17:54:58 +0100

    Address.java

--- Code/src/openwallet/bean/Address.java
+++ Code/src/openwallet/bean/Address.java
@@ -27,22 +27,24 @@ public class Address {
   private int id_user;
   private String firstname_receiver;
   private String lastname_receiver;
+  private String mobile_receiver;
   private String street;
   private String city;
   private String country;
-  private String codepost;
+  private String post_address;
 
   public Address(){};
 
-  public Address(int id_address, int id_user, String firstname_receiver, String lastname_receiver, String street, String city, String country, String codepost) {
+  public Address(int id_address, int id_user, String firstname_receiver, String lastname_receiver, String mobile_receiver, String street, String city, String country, String post_address) {
     this.id_address = id_address;
     this.id_user = id_user;
     this.firstname_receiver = firstname_receiver;
     this.lastname_receiver = lastname_receiver;
+    this.mobile_receiver = mobile_receiver;
     this.street = street;
     this.city = city;
     this.country = country;
-    this.codepost = codepost;
+    this.post_address = post_address;
   }
 
   public int getId_address() {
