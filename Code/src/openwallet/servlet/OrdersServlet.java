a72af2d1b49f930e0fabbf4fd872994e4fd94d3c
Lamela <harry81@qq.com>
2018-01-19 14:52:31 +0100

    Orders modified.

---
+++ Code/src/openwallet/servlet/OrdersServlet.java
@@ -0,0 +1,55 @@
+package openwallet.servlet;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import openwallet.bean.Orders;
+import openwallet.dao.OrdersDAO;
+import openwallet.util.Page;
+
+public class OrderServlet extends BaseBackServlet {
+
+	
+	public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+		return null;
+	}
+
+	
+	public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+		return null;
+	}
+	public String delivery(HttpServletRequest request, HttpServletResponse response, Page page) {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Orders o = ordersDAO.get(id);
+		o.setDelivery_date(new Date());
+		o.setStatus(OrdersDAO.waitConfirm);
+		ordersDAO.update(o);
+		return "@admin_order_list";
+	}
+
+	
+	public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+		return null;	
+	}
+
+	
+	public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+		return null;
+	}
+
+	
+	public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+		List<Orders> os = ordersDAO.list(page.getStart(),page.getCount());
+		itemOrderDAO.fill(os);
+		int total_number = orderDAO.getTotal_number();
+		page.setTotal(total_number);
+		
+		request.setAttribute("os", os);
+		request.setAttribute("page", page);
+		
+		return "admin/listOrder.jsp";
+	}
+}
