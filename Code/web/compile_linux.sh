50fffcd24095b56a508881f21e74d2a13ae09d40
XU Jitao <jitao.xu@insa-rouen.fr>
2018-01-20 19:20:31 +0100

    add registerSuccess.jsp

--- Code/web/compile_linux.sh
+++ Code/web/compile_linux.sh
@@ -4,7 +4,7 @@ javac -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes ../src/openwallet/util
 javac -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes ../src/openwallet/bean/*.java
 javac -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes ../src/openwallet/dao/*.java
 javac -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes ../src/openwallet/filter/*.java
-javac -Xlint:deprecation -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes ../src/openwallet/servlet/*.java
+javac -Xlint:deprecation -Xlint:unchecked -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes ../src/openwallet/servlet/*.java
 
-jar cf Openwallet.war WEB-INF include admin css fonts images js style.css
+jar cf Openwallet.war WEB-INF include admin css fonts images js style.css img *.jsp
 cp Openwallet.war ~/Downloads/apache-tomcat-8.5.24/webapps
