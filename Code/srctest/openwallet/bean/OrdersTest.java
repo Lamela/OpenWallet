57ca307d4f6b3e5ca9a42a0bd81c096a0a34edae
Rasendrasoa <sandratra.rasendrasoa@insa-rouen.fr>
2018-01-20 11:23:47 +0100

    Test Orders Done

---
+++ Code/srctest/openwallet/bean/OrdersTest.java
@@ -0,0 +1,83 @@
+package openwallet.bean;
+
+import openwallet.bean.Orders;
+import openwallet.dao.OrdersDAO;
+import org.junit.*;
+
+public class OrdersTest {
+  private OrdersDAO ordersDAO;
+  private Orders orders;
+  private int id_orders;
+  private User user;
+  private Address address;
+  private List<ItemOrder> itemOrders;
+
+  @Before
+  public void avantTest(){
+    this.itemOrders = null;
+    this.user = new User("Open","Wallet",new Date(0).valueOf("1995-11-21"),
+    "normalType", false, "open.wallet@gmail.com", "mdp", "0611111111", "123456");
+    this.address = new Address(myUser,"Jean",
+     "Dupont", "0611111111", "Rue République","Rouen", "France", "76000");
+     this.orders = new Orders(this.user, 100.0, this.address, this.address, "user_message", new Date(0).valueOf("2017-11-21"), new Date(0).valueOf("2017-11-21"), new Date(0).valueOf("2017-11-21"), new Date(0).valueOf("2017-11-21"),"Finished", 10, this.itemOrders);
+
+    this.ordersDAO = new OrdersDAO();
+    this.id_orders = this.ordersDAO.add(this.orders);
+  }
+
+  @Test
+  public void testAdd() {
+    Orders orders2= this.ordersDAO.get(this.id_orders);
+
+    assertEquals(this.assertOrdersEquals(this.orders, orders2), true);
+  }
+
+  @Test
+  public void testUpdate(){
+    this.orders.setCreate_date_order(new Date(0).valueOf("2018-01-18"));
+    this.ordersDAO.update(this.orders);
+    Orders orders2= ordersDAO.get(this.id_orders);
+
+    assertEquals(orders2.getCreate_date_order().equals(new Date(0).valueOf("2018-01-18")), true);
+  }
+
+  @Test
+  public void testDelete(){
+    Orders orders2= ordersDAO.get(this.id_orders);
+
+    id_orders2 = ordersDAO.add(orders2);
+    ordersDAO.delete(id_orders2);
+
+    assertEquals(ordersDAO.get(id_orders2), null);
+  }
+
+  private boolean assertOrdersEquals(Orders orders1,Orders orders2) {
+
+    if(orders1.getId_order()!=orders2.getId_order()) {
+      System.out.println("TestOrders Error: id_order not equals !");
+    } else {
+      if(!orders1.getUser().getId_user().equals(orders2.getUser().getId_user())) {
+        System.out.println("TestOrders Error: User not equals !");
+      }else {
+        if(!orders1.getStatus().equals(orders2.getStatus())) {
+          System.out.println("TestOrders Error: status not equals !");
+        } else {
+          if(!orders1.getTotal_price().equals(orders2.getTotal_price())) {
+            System.out.println("TestOrders Error: total_price not equals !");
+          } else {
+            if(!orders1.getInvoice_address().getId_address().equals(orders2.getInvoice_address().getId_address())) {
+              System.out.println("TestOrders Error: invoice_address not equals !");
+            } else {
+              if(!orders1.getDelivery_address().getId_address().equals(orders2.getDelivery_address().getId_address())) {
+                System.out.println("TestOrders Error: delivery_address not equals !");
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return equal;
+
+  }
+}
