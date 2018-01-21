8cfd8944c7d515cfb2419d714fe6f88b05d57516
XU Jitao <jitao.xu@insa-rouen.fr>
2018-01-21 10:24:19 +0100

    modify servlet

--- Code/src/openwallet/servlet/ForeServlet.java
+++ Code/src/openwallet/servlet/ForeServlet.java
@@ -138,6 +138,7 @@ public class ForeServlet extends BaseForeServlet {
 		
 		Category c = new CategoryDAO().get(cid);
 		new ProductDAO().fill(c);
+		ArrayList<Product> ps = c.getProducts();
 		// new ProductDAO().setSaleAndReviewNumber(c.getProducts());		
 	/*	
 		String sort = request.getParameter("sort");
@@ -165,6 +166,7 @@ public class ForeServlet extends BaseForeServlet {
 		}
 	*/	
 		request.setAttribute("c", c);
+		request.setAttribute("ps", ps);
 		return "category.jsp";		
 	}
 	
